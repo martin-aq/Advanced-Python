135bbcb2e83d5ee34026513361e24feee7cb4753
Martín Aldana <aldanaq.martin@gmail.com>
2023-04-25 11:35:14 -0700

    Updated CSV Files Folder

--- 5_CVS-Files/crear_csv.py
+++ 5_CVS-Files/crear_csv.py
@@ -1,7 +1,15 @@
+"""This program shows how to create a CSV File using the CSV Library"""
+
 import csv
+import os
 
 ruta = "csv_vacio.csv"
+ruta_absoluta = "C:\\Users\\marti\\OneDrive\\Escritorio\\PythonAvanzado\\5_CVS-Files\\csv_vacio.csv"
+ruta_absoluta_os = os.path.join(os.getcwd(), "csv_vacio.csv")
 
-archivo_abierto = open(ruta, "w")
-writer = csv.writer(archivo_abierto, delimiter=",")
-archivo_abierto.close()
+print(ruta_absoluta)
+print(ruta_absoluta_os)
+
+# archivo_abierto = open(ruta, "w")
+# writer = csv.writer(archivo_abierto, delimiter=",")
+# archivo_abierto.close()
---
+++ 5_CVS-Files/escribir_csv_files.py
@@ -0,0 +1,21 @@
+import csv
+
+columnas = ["nombre", "apellido", "edad"]
+dato = ["Paco", "Botero", 26]
+
+datos_lista = [
+    ["Paco", "Botero", 26],
+    ["Javier", "Quiñonez", 24],
+    ["Emilio", "Tafur", 25]
+]
+
+datos_dict = [
+    {"nombre": "Paco", "apellido": "Botero", "edad": 26},
+    {"nombre": "Javier", "apellido": "Quiñonez", "edad": 24},
+    {"nombre": "Emilio", "apellido": "Tafur", "edad": 25}
+]
+
+with open("datos.csv", "w", newline="") as archivo:
+    writer = csv.writer(archivo, delimiter=",")
+    writer.writerow(columnas)
+    writer.writerows(datos_lista)
