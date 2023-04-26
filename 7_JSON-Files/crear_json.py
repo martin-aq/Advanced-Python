82b48f27bdc219cad0430efc57a8b2958be2d350
Martín Aldana <aldanaq.martin@gmail.com>
2023-04-25 19:29:11 -0700

    Modified crear_json.py and persona.json

--- 7_JSON-Files/crear_json.py
+++ 7_JSON-Files/crear_json.py
@@ -6,6 +6,7 @@ persona = {
     "edad": 24
 }
 
-objeto_json = json.dumps(persona, indent=2)
+# objeto_json = json.dumps(persona, indent=2)
 with open("persona.json", "w") as archivo_json:
-    archivo_json.write(objeto_json)
+    # archivo_json.write(objeto_json)
+    json.dump(persona, archivo_json)
