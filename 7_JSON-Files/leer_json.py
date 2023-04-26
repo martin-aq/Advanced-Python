144f5644f73725c5a12d1e9e1d30ccb9cffacdc2
Martín Aldana <aldanaq.martin@gmail.com>
2023-04-25 19:50:21 -0700

    Updated JSON Files Folder

---
+++ 7_JSON-Files/leer_json.py
@@ -0,0 +1,10 @@
+"""This program shows how to read a JSON file"""
+
+import json
+
+with open("persona.json", "r") as archivo_json:
+    datos_json = json.load(archivo_json)
+
+print(type(datos_json))
+print(datos_json)
+print(datos_json["nombre"])
